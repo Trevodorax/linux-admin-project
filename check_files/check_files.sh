527d53fd8897953cce9fa06c1408f91d2d8ffa9f
Mil0W0 <lhilderal@myges.fr>
2023-07-06 00:11:58 +0200

    all good for suid script

--- check_files/check_files.sh
+++ check_files/check_files.sh
@@ -1,44 +1,70 @@
 #!/bin/bash
 
 #FIND FILES : -perm for permissions and we want suid and sgid then
-echo "# EXECUTABLE FILES WITH SUID OR SGID RIGHTS" > list_of_files.txt 
-find / -type f -executable -perm -4000 >> list_of_files.txt 2> /dev/null
+#echo "# EXECUTABLE FILES WITH SUID OR SGID RIGHTS" > list_of_files.txt 
+find / -type f -executable -perm -4000 2> /dev/null > list_of_files.txt
 find / -type f -executable -perm -2000  >> list_of_files.txt 2> /dev/null
 
-file="list_of_files.txt"
+new_file="list_of_files.txt"
 old_file="last_list_of_files.txt"
 
-#CHECK DIFF BETWEEN OLD AND NEW FILES
-# -f = normal file -s = exist and not empty
+declare -A files 
+declare -A copy_files
 
-if [ -f $file -a -f $old_file ]
+while IFS= read -r file
+do
+	files["$file"]=1
+	copy_files["$file"]=1
+
+done < "$new_file"
+
+
+
+if [ -f "$old_file" ]
 then
-	diff=$(diff $file $old_file | grep -E "^< " | cut -d' ' -f2)
-	if [ -n "$diff" ]	
+	declare -A old_files
+	while IFS= read -r file
+	do
+
+		old_files["$file"]=1
+	done < "$old_file" 
+
+	for file in ${!old_files[*]}
+	do
+		unset files["$file"]
+	done
+
+	for file in ${!copy_files[*]}
+	do
+		unset old_files["$file"]
+	done
+
+
+	if [ "${#files[*]}" -gt 0 ]
 	then
-		echo -e "\e[31m WARNING: DIFFERENCES FOUND ON :\e[0m"
-		echo $diff
-		last_line=""
-		while IFS= read -r modified_file; do
-			#GET THE FILE LAST MODIFICATION DATE WITH FORMAT DATE
-			if [ -f $modified_file ]
-			then
-
-				if [ $last_file != $modified_file ]
-				then
-					date=$(stat -c%y $modified_file)
-					echo "$modified_file modified on : $date"
-				fi
-			fi
-			last_file=$modified_file
-		done <<< $diff	
-	else 
-		echo -e "\033[0;32mNo changes since last launch of the script !\033[0m"
-		echo "Check $file."
+		echo -e "\033[0;31mWARNING - DIFFERENCES FOUND:\033[0m "
+		for file in "${!files[*]}"
+		do
+			 date=$(stat -c%y "$file")
+			 echo "$date : $file"
+		done
 	fi
+
+         if [ "${#old_files[*]}" -gt 0 ]
+	then
+		echo  -e "\033[0;31m WARNING - DIFFERENCES FOUND:\033[0m"
+		for file in "${!old_files[*]}"
+		do
+			date=$(stat -c%y "$file")
+			echo "$date : $file"
+		done
+	
+	 fi
+
+	 if [ "${#old_files[*]}" -eq 0 -o "${#files[*]}" -eq 0 ]
+	 then
+		echo -e  "\033[0;32mNo changes.\033[0m"
+	 fi
+		
 fi
-
-#KEEP THE FILE FOR NEXT LAUNCH
-cp $file $old_file
-
-#echo -e "\e[31mTEST TEST TEST\e[0m"
+cp list_of_files.txt last_list_of_files.txt
